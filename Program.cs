5392e65a7f4568205df5df0db63e70cba4d56bae
SNasser97 <snasserahmed@outlook.com>
2020-12-30 19:03:29 +0000

    Complete stopwatch exercise

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Exercises
@@ -10,6 +11,27 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
+            var stopwatch = new StopWatch(); // 00:00:00
+            stopwatch.StartTimer();
+            for (int i = 0; stopwatch.IsRunning; i++)
+            {
+                Console.WriteLine("Press Enter each time to get current duration");
+                // Display every 1s
+                Thread.Sleep(1_000);
+                Console.Write("Type quit or q to exit:");
+                var input = Console.ReadLine().ToLower().Trim();
+                //getting stopwatch time here..
+                stopwatch.Interval();
+
+                if (String.Equals("quit", input) || input.Equals("q"))
+                {
+                    stopwatch.EndTimer();
+                }
+
+            }
+
+            Console.WriteLine("Exited program");
+            Console.WriteLine("Final duration: {0}", stopwatch.Duration);
         }
     }
 }
--- StopWatch.cs
+++ StopWatch.cs
@@ -1,7 +1,59 @@
-﻿namespace Exercises
+﻿using System;
+
+namespace Exercises
 {
     public class StopWatch
     {
+        // Exercise create a stop watch
+        // Job is to simulate stopwatch behaviour. 2 Methods.
+        // method 1 -> start
+        // method 2 -> stop
+        // Ask Duration of time between start/stop -> TIMESPAN
+        // Able to use this stop watch many times
+        // so start/stop then start/stop. Cannot start twice! - throw error exception
+
+        private static int _numTimesInvoked = 0;
+        public bool IsRunning { get; private set; }
+        private DateTime _timer;
+        public DateTime CurrentTime { get; private set; }
+        public TimeSpan Duration { get; private set; }
+        public DateTime Timer
+        {
+            get { return _timer; }
+            set { _timer = value; }
+        }
+
+        public void StartTimer()
+        {
+            IsRunning = true;
+            // new timer each time method is called
+            // reset timespan when method recalled
+            Timer = DateTime.Now;
+            Duration = new TimeSpan(0,0,0);
+            // Can only invoke start once
+            HandleInvoke(_numTimesInvoked += 1);
+        }
+
+        static void HandleInvoke(int calls)
+        {
+            if (calls > 1)
+                throw new InvalidOperationException("Cannot invoke start twice!");
+        }        
+
+        public void Interval()
+        {
+            CurrentTime = DateTime.Now;
+            Duration = CurrentTime - Timer;
+            Console.WriteLine("Duration: {0}", Duration);
+        }
+
+        public void EndTimer()
+        {
+            IsRunning = false;
+            // Reset as start/stop should allow starting again.
+            HandleInvoke(_numTimesInvoked = 0);
+        }
+
 
     }
 }
