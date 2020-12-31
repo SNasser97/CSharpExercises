c38b9700150c95deacd06be35c83d64014716320
SNasser97 <snasserahmed@outlook.com>
2020-12-31 14:47:34 +0000

    Create class to handle field and methods of post + class dict collection of posts

---
+++ Posts.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Exercises
+{
+    public class Posts
+    {
+        private readonly Dictionary<int,Post> _postList;
+        public Dictionary<int,Post> PostList {
+            get
+            {
+                return _postList;
+            }
+        }
+        public Posts()
+        {
+            _postList = new Dictionary<int, Post>();
+        }
+    }
+}
